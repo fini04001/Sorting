cb0d5864b8cf5893a10a154a3f1fe6dcadc8e0bc
fini04001 <92377261+fini04001@users.noreply.github.com>
2022-02-18 15:42:19 +0100

    Add files via upload

---
+++ Insertion sorting/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+
+
+namespace Template
+{
+    class Program
+    {
+        static public void Insertionsort(int[] insr) //place changer
+        //Sorting type
+        {
+            int key, j;
+
+            for (int i = 0; i < insr.Length; i++)
+            {
+                key = insr[i];
+                j = i - 1;
+
+                while (j >= 0 && insr[j] > key)
+                {
+                    insr[j + 1] = insr[j];
+                    j = j - 1;
+                }
+                insr[j + 1] = key;
+            }
+            //sorting type
+        }         static void Main(string[] args)  
+          //Scrambler
+        {
+            int length = 1000;
+            int[] scrm = new int[length];
+
+            for (int s = 0; s <= length - 1; s++)
+            {
+                scrm[s] = s + 1;
+            }
+            Random random = new Random();
+            scrm = scrm.OrderBy(x => random.Next()).ToArray();
+            Console.WriteLine("Scrambled: ");
+            foreach (var e in scrm)
+            {
+                Console.WriteLine(e);
+            }
+            //Scrambler
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            //Sortering Funktion
+            
+
+            Insertionsort(scrm);
+     
+            
+            //Sortering Funktion
+
+            stopwatch.Stop();
+
+            Console.WriteLine("Sorted: ");
+            //in Funktion
+            foreach(var e in scrm) 
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("Size of sort: " + length);
+            Console.WriteLine("Sorting time: {0} ms", stopwatch.Elapsed.Milliseconds);          
+        }   
+    }
+}
